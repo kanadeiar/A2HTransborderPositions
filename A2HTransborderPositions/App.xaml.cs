6eab3df8bd461e341dc1675af33b18a726fd4b43
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-15 12:21:25 +0400

    Инфраструктура приложения +

--- A2HTransborderPositions/App.xaml.cs
+++ A2HTransborderPositions/App.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using A2HTransborderPositions.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
 
 namespace A2HTransborderPositions
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static IServiceProvider __Services;
+        private static IServiceCollection GetServices()
+        {
+            var services = new ServiceCollection();
+            InitializeServices(services);
+            return services;
+        }
+        private static void InitializeServices(IServiceCollection services)
+        {
+            services.AddScoped<MainWindowViewModel>();
+        }
+        /// <summary> Сервисы </summary>
+        public static IServiceProvider Services => __Services ??= GetServices()
+            .BuildServiceProvider();
     }
 }
---
+++ A2HTransborderPositions/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using A2HTransborderPositions.Commands;
+using A2HTransborderPositions.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace A2HTransborderPositions.ViewModel
+{
+    class MainWindowViewModel : Base.ViewModel
+    {
+        #region Data
+
+
+
+        #endregion Data
+
+        #region Properties
+
+
+
+
+
+        #region Support
+
+        private string _Title = "A2H Утилита настройки позиций трансбордера";
+        /// <summary> Заголовок приложения </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion Support
+
+        #endregion Properties
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        #region Commands
+
+
+
+
+        #region Support
+
+        private ICommand _CloseApplicationCommand;
+
+        /// <summary> Закрыть приложение </summary>
+        public ICommand CloseApplicationCommand => _CloseApplicationCommand ??=
+            new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion Support
+
+        #endregion Commands
+    }
+}
