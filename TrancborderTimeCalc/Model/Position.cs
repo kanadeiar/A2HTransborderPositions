250926f7bd41d88663b75424162add03d8286416
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-01-19 10:22:59 +0400

    Базовая разметка окна

--- TrancborderTimeCalc/Model/Position.cs
+++ TrancborderTimeCalc/Model/Position.cs
@@ -25,5 +25,37 @@ public class Position : Entity
         get => _Massif;
         set => Set(ref _Massif, value);
     }
+
+    private bool _IsInput;
+    /// <summary> Вход в камеру созревания </summary>
+    public bool IsInput
+    {
+        get => _IsInput;
+        set => Set(ref _IsInput, value);
+    }
+
+    private bool _IsOutput;
+    /// <summary> Выход из камеры созревания </summary>
+    public bool IsOutput
+    {
+        get => _IsOutput;
+        set => Set(ref _IsOutput, value);
+    }
+
+    private bool _IsHitTable1;
+    /// <summary> Ударный стол 1 </summary>
+    public bool IsHitTable1
+    {
+        get => _IsHitTable1;
+        set => Set(ref _IsHitTable1, value);
+    }
+
+    private bool _IsHitTable2;
+    /// <summary> Ударный стол 2 </summary>
+    public bool IsHitTable2
+    {
+        get => _IsHitTable2;
+        set => Set(ref _IsHitTable2, value);
+    }
 }
 
